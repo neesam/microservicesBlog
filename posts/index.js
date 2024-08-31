a061d25eafada18c96d581919aefcf06b53da5b5
anees <anees@spins.com>
2024-08-31 16:10:36 -0600

    Added post and get requests to comments

--- posts/index.js
+++ posts/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
-app.use(bodyParser);
+app.use(bodyParser.json());
 
 const posts = {};
 
