ad1f854c689ec324b0e00c341e0c7b548faf1af3
anees <anees@spins.com>
2024-09-05 22:01:36 -0600

    Comment moderation events

--- query/index.js
+++ query/index.js
@@ -3,37 +3,48 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+  const { type, data } = req.body;
 
-    if (type === 'PostCreated') {
-        const { id, title } = data;
+  if (type === 'PostCreated') {
+    const { id, title } = data;
 
-        posts[id] = { id, title, comments: [] };
-    }
+    posts[id] = { id, title, comments: [] };
+  }
 
-    if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
 
-        const post = posts[postId];
-        post.comments.push({ id, content });
-    }
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
 
-    console.log(posts);
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
 
-    res.send({});
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  console.log(posts);
+
+  res.send({});
 });
 
 app.listen(8002, () => {
-    console.log('Listening on 8002');
+  console.log('Listening on 8002');
 });
